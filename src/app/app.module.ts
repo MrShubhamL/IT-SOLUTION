4097e1adf8cd87bd47e2dda9b6b5633b582cc104
Shubham Lohar <mr.shubhamlohar952@gmail.com>
2024-11-22 12:23:48 +0530

    changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/utilities/navbar/navbar.component';
+import { FooterComponent } from './components/utilities/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
